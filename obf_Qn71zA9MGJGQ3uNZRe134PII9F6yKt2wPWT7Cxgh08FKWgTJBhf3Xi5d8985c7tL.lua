79c6bf40afc8109030bf355b4ed21f116003744e
BINjiaobzx6 <3766961965@qq.com>
2024-06-22 13:11:48 +0800

    Add files via upload

---
+++ obf_Qn71zA9MGJGQ3uNZRe134PII9F6yKt2wPWT7Cxgh08FKWgTJBhf3Xi5d8985c7tL.lua
@@ -0,0 +1,22 @@
+local library   = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/random%202"))()
+local One = library:Window("空脚本EMPTY")
+
+One:Toggle("人物白色ESP并且显示人名", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP'))()
+end)
+
+One:Toggle("人物锁头", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/6RQGbFqD"))()
+end)
+
+One:Toggle("人物范围", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/jiNwDbCN"))()
+end)
+
+One:Toggle("无限跳", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
+end)
+
+One:Toggle("全地图高亮", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BINjiaobzx6/BINjiao/main/%E6%89%80%E4%BB%A5%E4%B8%9C%E8%A5%BF.txt"))()
+end)
