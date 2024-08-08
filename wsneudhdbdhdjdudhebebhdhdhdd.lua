c02ac6037336cdc110eb12261f290d3ea1afd548
BINjiaobzx6 <3766961965@qq.com>
2024-08-08 16:56:18 +0800

    Add files via upload

--- wsneudhdbdhdjdudhebebhdhdhdd.lua
+++ wsneudhdbdhdjdudhebebhdhdhdd.lua
@@ -998,33 +998,6 @@ Callback = function(val)
 end  
 })
 
-QYTab:AddToggle({
-Name = "人物穿墙",
-Default = false,
-Callback = function(Value)
-        if Value then
-		    Noclip = true
-		    Stepped = game.RunService.Stepped:Connect(function()
-			    if Noclip == true then
-				    for a, b in pairs(game.Workspace:GetChildren()) do
-                        if b.Name == game.Players.LocalPlayer.Name then
-                            for i, v in pairs(game.Workspace[game.Players.LocalPlayer.Name]:GetChildren()) do
-                                if v:IsA("BasePart") then
-                                    v.CanCollide = false
-                                end
-                            end
-                        end
-                    end
-			    else
-				    Stepped:Disconnect()
-			    end
-		    end)
-	    else
-		    Noclip = false
-	    end
-end    
-})
-
 QYTab:AddButton({
 Name = "删除小蜘蛛",
 Callback = function()
